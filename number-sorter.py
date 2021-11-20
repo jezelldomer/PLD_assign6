62bae76e34d684d0d94940030c0f38a3569ae7d3
Jezell <jzlldomer@gmail.com>
2021-11-21 01:30:56 +0800

    Inputs

---
+++ number-sorter.py
@@ -0,0 +1,6 @@
+#Create a program that ask 4 numbers
+def inputs():
+    num1 = float(input("Enter your first number: "))
+    num2 = float(input("Enter your second number: "))
+    num3 = float(input("Enter your third number: "))
+    num4 = float(input("Enter your fourth number: "))
