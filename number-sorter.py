f38927d4324a4217f7f990eb33151d2de4044c9a
Jezell <jzlldomer@gmail.com>
2021-11-21 16:26:09 +0800

    if-else

--- number-sorter.py
+++ number-sorter.py
@@ -1,6 +1,19 @@
 #Create a program that ask 4 numbers
-def inputs():
-    num1 = float(input("Enter your first number: "))
-    num2 = float(input("Enter your second number: "))
-    num3 = float(input("Enter your third number: "))
-    num4 = float(input("Enter your fourth number: "))
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+num3 = float(input("Enter your third number: "))
+num4 = float(input("Enter your fourth number: "))
+
+def high_low(num1, num2, num3, num4):
+ if num1 >= num2 and num1 >= num3 and num1 >= num4:
+        if num2 >= num3 and num2 >= num4:
+            if num3 >= num4:
+                _numbers = [num1, num2, num3, num4]
+                print(f"The numbers are in the following order:{_numbers} ")
+            elif num4 >= num3:
+                _numbers = [num1, num2, num4, num3]
+                print(f"The numbers are in the following order:{_numbers} ")
+        elif num3 >= num2 and num3 >= num4:
+            if num2 >= num4:
+                _numbers = [num1, num3, num2, num4]
+                print(f"The numbers are in the following order:{_numbers} ")
