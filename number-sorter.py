0f4ac3dcfbb081dd2ae5e8722dcdb9d5ec4035c9
Jezell <jzlldomer@gmail.com>
2021-11-21 16:59:13 +0800

    if-else continuation 2

--- number-sorter.py
+++ number-sorter.py
@@ -32,3 +32,28 @@ def high_low(num1, num2, num3, num4):
             if num3 >= num4:
                 _numbers = [num2, num1, num3, num4]
                 print(f"The numbers are in the following order:{_numbers} ")
+elif num4 >= num3:
+                _numbers = [num2, num1, num4, num3]
+                print(f"The numbers are in the following order:{_numbers} ")
+        elif num3 >= num1 and num3 >= num4:
+            if num1 >= num4:
+                _numbers = [num2, num3, num1, num4]
+                print(f"The numbers are in the following order:{_numbers} ")
+            elif num4 >= num1:
+                _numbers = [num2, num3, num4, num1]
+                print(f"The numbers are in the following order:{_numbers} ")
+        elif num4 >= num1 and num4 >= num3:
+            if num1 >= num3:
+                _numbers = [num2, num4, num1, num3]
+                print(f"The numbers are in the following order:{_numbers} ")
+            elif num3 >= num1:
+                _numbers = [num2, num4, num3, quan1]
+                print(f"The numbers are in the following order:{_numbers} ")
+    elif num3 >= num1 and num3 >= num2 and num3 >= num4:
+        if num1 >= num2 and num1 >= num4:
+            if num2 >= num4:
+                _numbers = [num3, num1, num2, num4]
+                print(f"The numbers are in the following order:{_numbers} ")
+            elif num4 >= num2:
+                _numbers = [num3, num1, num4, num2]
+                print(f"The numbers are in the following order:{_numbers} ")
