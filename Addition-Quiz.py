e671d2afc731c449f7f97bb4b9d94b249c0eaef6
Jezell <jzlldomer@gmail.com>
2021-11-21 19:50:36 +0800

    Greetings and input of name

---
+++ Addition-Quiz.py
@@ -0,0 +1,9 @@
+#Create a program that automatically generate two random numbers to add (0 to 99)
+#Let the user answer and evaluate if the user has the correct answer
+#The program will ask the user 10 addition operation
+
+import random
+
+print (f"Good day! There will be an Addition Quiz today. ")
+name = input("Please enter your name: ")
+print (f"This is your 10 Item Quiz, {name}. Good Luck! ")
